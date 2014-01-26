d4a76deedbdf5acc011ca2921122e1b6ff6c378f
Matt Jibson <matt.jibson@gmail.com>
2014-01-26 00:08:56 -0500

    Initial

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var f = flag.String("f", "", "Output file, else stdout.")
+var p = flag.String("p", "main", "Package.")
+
+var fRE = regexp.MustCompile("[a-zA-Z0-9_]+")
+
+func main() {
+	flag.Parse()
+	w := os.Stdout
+	var err error
+	if *f != "" {
+		if w, err = os.Create(*f); err != nil {
+			log.Fatal(err)
+		}
+		defer w.Close()
+	}
+	fmt.Fprintf(w, "package %s", *p)
+	for _, fname := range flag.Args() {
+		b, err := ioutil.ReadFile(fname)
+		if err != nil {
+			log.Fatal(err)
+		}
+		v := strings.Join(fRE.FindAllString(fname, -1), "_")
+		fmt.Fprintf(w, "\n\nvar %s = []byte(%q)", v, b)
+	}
+}
