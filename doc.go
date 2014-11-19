7948e41d098c38db638cec7721f34fc85ec48cd0
Matt Jibson <matt.jibson@gmail.com>
2014-11-19 03:21:44 -0500

    Convert files to a http.FileSystem interface
    Breaking change with previous version.

---
+++ doc.go
@@ -0,0 +1,28 @@
+/*
+
+esc embeds files into go programs and provides http.FileSystem interfaces
+to them.
+
+It adds all named files or files recursively under named directories at the
+path specified.
+
+
+Usage:
+	esc [-o outfile.go] [-pkg package] [-prefix prefix] [name ...]
+
+The flags are:
+	-o=""
+		output filename, defaults to stdout
+	-pkg="main"
+		package name of output file, defaults to main
+	-prefix=""
+		strip given prefix from filenames
+
+Accessing Embedded Files
+
+After producing an output file, the assets may be accessed with the FS()
+function, which takes a flag to use local assets instead (for local
+development).
+
+*/
+package main
--- main.go
+++ main.go
@@ -1,37 +1,211 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"regexp"
+	"path"
+	"path/filepath"
+	"sort"
 	"strings"
 )
 
-var f = flag.String("f", "", "Output file, else stdout.")
-var p = flag.String("p", "main", "Package.")
+var (
+	flagOut    = flag.String("o", "", "Output file, else stdout.")
+	flagPkg    = flag.String("pkg", "main", "Package.")
+	flagPrefix = flag.String("prefix", "", "Prefix to strip from filesnames.")
+)
 
-var fRE = regexp.MustCompile("[a-zA-Z0-9_]+")
+type file struct {
+	data  []byte
+	local string
+}
 
 func main() {
 	flag.Parse()
-	w := os.Stdout
 	var err error
-	if *f != "" {
-		if w, err = os.Create(*f); err != nil {
+	var fnames []string
+	content := make(map[string]file)
+	prefix := filepath.ToSlash(*flagPrefix)
+	for _, base := range flag.Args() {
+		files := []string{base}
+		for len(files) > 0 {
+			fname := files[0]
+			files = files[1:]
+			f, err := os.Open(fname)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fi, err := f.Stat()
+			if err != nil {
+				log.Fatal(err)
+			}
+			if fi.IsDir() {
+				fis, err := f.Readdir(0)
+				if err != nil {
+					log.Fatal(err)
+				}
+				for _, fi := range fis {
+					files = append(files, filepath.Join(fname, fi.Name()))
+				}
+			} else {
+				b, err := ioutil.ReadAll(f)
+				if err != nil {
+					log.Fatal(err)
+				}
+				n := strings.TrimPrefix(filepath.ToSlash(fname), prefix)
+				local := path.Join(filepath.ToSlash(base), n)
+				n = path.Join("/", n)
+				content[n] = file{data: b, local: local}
+				fnames = append(fnames, n)
+			}
+			f.Close()
+		}
+	}
+	sort.Strings(fnames)
+	w := os.Stdout
+	if *flagOut != "" {
+		if w, err = os.Create(*flagOut); err != nil {
 			log.Fatal(err)
 		}
 		defer w.Close()
 	}
-	fmt.Fprintf(w, "package %s\n", *p)
-	for _, fname := range flag.Args() {
-		b, err := ioutil.ReadFile(fname)
-		if err != nil {
+	fmt.Fprintf(w, header, *flagPkg)
+	for _, fname := range fnames {
+		f := content[fname]
+		var buf bytes.Buffer
+		gw := gzip.NewWriter(&buf)
+		if _, err := gw.Write(f.data); err != nil {
 			log.Fatal(err)
 		}
-		v := strings.Join(fRE.FindAllString(fname, -1), "_")
-		fmt.Fprintf(w, "\nvar %s = []byte(%q)\n", v, b)
+		if err := gw.Close(); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "\n\t%q: {compressed: %q, size: %v, local: %q},\n", fname, buf.String(), len(f.data), f.local)
 	}
+	fmt.Fprint(w, footer)
 }
+
+const (
+	header = `package %s
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"sync"
+	"time"
+)
+
+type localFS struct{}
+
+var local localFS
+
+type staticFS struct{}
+
+var static staticFS
+
+type file struct {
+	compressed string
+	size       int64
+	local      string
+
+	data []byte
+	once sync.Once
+	name string
+}
+
+func (_ localFS) Open(name string) (http.File, error) {
+	f, present := data[name]
+	if !present {
+		return nil, os.ErrNotExist
+	}
+	return os.Open(f.local)
+}
+
+func (_ staticFS) Open(name string) (http.File, error) {
+	f, present := data[name]
+	if !present {
+		return nil, os.ErrNotExist
+	}
+	var err error
+	f.once.Do(func() {
+		var gr *gzip.Reader
+		gr, err = gzip.NewReader(bytes.NewBufferString(f.compressed))
+		if err != nil {
+			return
+		}
+		f.data, err = ioutil.ReadAll(gr)
+		f.name = path.Base(name)
+	})
+	return f.File()
+}
+
+func (f *file) File() (http.File, error) {
+	hf := httpFile{
+		Reader: bytes.NewReader(f.data),
+		file:   f,
+	}
+	return &hf, nil
+}
+
+type httpFile struct {
+	*bytes.Reader
+	*file
+}
+
+func (f *file) Close() error {
+	return nil
+}
+
+func (f *file) Readdir(count int) ([]os.FileInfo, error) {
+	return nil, nil
+}
+
+func (f *file) Stat() (os.FileInfo, error) {
+	return f, nil
+}
+
+func (f *file) Name() string {
+	return f.name
+}
+
+func (f *file) Size() int64 {
+	return f.size
+}
+
+func (f *file) Mode() os.FileMode {
+	return 0
+}
+
+func (f *file) ModTime() time.Time {
+	return time.Time{}
+}
+func (f *file) IsDir() bool {
+	return false
+}
+func (f *file) Sys() interface{} {
+	return f
+}
+
+// FS returns a http.Filesystem for the embedded assets. If local is true,
+// the filesystem's contents are instead used.
+func FS(useLocal bool) http.FileSystem {
+	if useLocal {
+		return local
+	}
+	return static
+}
+
+var data = map[string]*file{
+`
+	footer = `}
+`
+)
